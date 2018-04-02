41486f266e953dda93deb38b2b177bf86bee2161
siddharthnishtala <siddharth.nishtala@gmail.com>
2018-04-02 15:17:07 +0530

    Improved sign in functionality
    Signed-off-by: siddharthnishtala <siddharth.nishtala@gmail.com>

--- registration/views.py
+++ registration/views.py
@@ -3,6 +3,7 @@ from django.views.generic import TemplateView
 from django.contrib.auth.models import User
 from django.contrib.auth import login, authenticate
 from django.contrib import messages
+from django.core import serializers
 
 
 class LandingPage(TemplateView):
@@ -11,7 +12,19 @@ class LandingPage(TemplateView):
 
 class HomePage(TemplateView):
     def get(self, request, **kwargs):
-        return render(request, 'home.html', context=None)
+
+        username = request.session.get("username", None)
+        if username is None:
+            messages.error(request, "Please sign up or login!")
+            return render(request, 'signup.html', context=None)
+
+        user = User.objects.filter(username=username)[0]
+
+        if not user.is_authenticated:
+            messages.error(request, "Please sign up or login!")
+            return render(request, 'signup.html', context=None)
+        else:
+            return render(request, 'home.html', context={"user" : user})
 
     def post(self, request, **kwargs):
         email = request.POST.get("email")
@@ -29,9 +42,19 @@ class SignUpPage(TemplateView):
         return render(request, 'signup.html', context=None)
 
     def post(self, request, **kwargs):
+        fname = request.POST.get("firstname")
+        lname = request.POST.get("lastname")
         email = request.POST.get("user[email]")
         pass1 = request.POST.get("user[password]")
         pass2 = request.POST.get("user[password2]")
+
+        if not fname.isalpha():
+            messages.error(request, "First name can only contain alphabets!")
+            return render(request, 'signup.html', context=None)
+
+        if not lname.isalpha():
+            messages.error(request, "Last name can only contain alphabets!")
+            return render(request, 'signup.html', context=None)
 
         if User.objects.filter(username=email).exists():
             messages.error(request, "Email already registered!")
@@ -44,5 +67,8 @@ class SignUpPage(TemplateView):
         else:
             # Add user to database
             user = User.objects.create_user(email, email, pass1)
+            user.first_name = fname
+            user.last_name = lname
             user.save()
+            request.session["username"] = user.get_username()
             return redirect('HomePage')
