e7b0eaf43e73d574ee1c2a46e54df56b811c8daa
siddharthnishtala <siddharth.nishtala@gmail.com>
2018-04-04 22:27:42 +0530

    Added logout, login functionalities
    Signed-off-by: siddharthnishtala <siddharth.nishtala@gmail.com>

--- registration/urls.py
+++ registration/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('', views.LandingPage.as_view(), name='LandingPage'),
     path('signup/', views.SignUpPage.as_view(), name='SignUpPage'),
     path('home/', views.HomePage.as_view(), name='HomePage'),
+    path('logout/', views.logout_view, name='LogoutPage')
 ]
--- registration/views.py
+++ registration/views.py
@@ -1,13 +1,17 @@
 from django.shortcuts import render, redirect
 from django.views.generic import TemplateView
 from django.contrib.auth.models import User
-from django.contrib.auth import login, authenticate
+from django.contrib.auth import login, authenticate, logout
 from django.contrib import messages
 
 
 class LandingPage(TemplateView):
     def get(self, request, **kwargs):
-        return render(request, 'landing.html', context=None)
+        if not request.user.is_authenticated:
+            return render(request, 'landing.html', context=None)
+        else:
+            return render(request, 'landingAfterLogin.html', context={"user": request.user})
+
 
     def post(self, request, **kwargs):
         email = request.POST.get("email")
@@ -32,36 +36,58 @@ class HomePage(TemplateView):
 
 class SignUpPage(TemplateView):
     def get(self, request, **kwargs):
-        return render(request, 'signup.html', context=None)
+        if not request.user.is_authenticated:
+            return render(request, 'signup.html', context=None)
+        else:
+            messages.info(request, "Please sign out for accessing the login page!")
+            return redirect('HomePage')
 
     def post(self, request, **kwargs):
-        fname = request.POST.get("firstname")
-        lname = request.POST.get("lastname")
-        email = request.POST.get("user[email]")
-        pass1 = request.POST.get("user[password]")
-        pass2 = request.POST.get("user[password2]")
+        if request.POST['action'] == "Sign me up!":
+            fname = request.POST.get("firstname")
+            lname = request.POST.get("lastname")
+            email = request.POST.get("user[email]")
+            pass1 = request.POST.get("user[password]")
+            pass2 = request.POST.get("user[password2]")
 
-        if not fname.isalpha():
-            messages.error(request, "First name can only contain alphabets!")
-            return render(request, 'signup.html', context=None)
+            if not fname.isalpha():
+                messages.error(request, "First name can only contain alphabets!")
+                return render(request, 'signup.html', context=None)
 
-        if not lname.isalpha():
-            messages.error(request, "Last name can only contain alphabets!")
-            return render(request, 'signup.html', context=None)
+            if not lname.isalpha():
+                messages.error(request, "Last name can only contain alphabets!")
+                return render(request, 'signup.html', context=None)
 
-        if User.objects.filter(username=email).exists():
-            messages.error(request, "Email already registered!")
-            return render(request, 'signup.html', context=None)
+            if User.objects.filter(username=email).exists():
+                messages.error(request, "Email already registered!")
+                return render(request, 'signup.html', context=None)
 
-        if pass1 != pass2:
-            messages.error(request, "Password mismatch!")
-            return render(request, 'signup.html', context=None)
+            if pass1 != pass2:
+                messages.error(request, "Password mismatch!")
+                return render(request, 'signup.html', context=None)
 
-        else:
-            # Add user to database
-            user = User.objects.create_user(email, email, pass1)
-            user.first_name = fname
-            user.last_name = lname
-            user.save()
-            login(request, user)
-            return redirect('HomePage')
+            else:
+                # Add user to database
+                user = User.objects.create_user(email, email, pass1)
+                user.first_name = fname
+                user.last_name = lname
+                user.save()
+                login(request, user)
+                return redirect('HomePage')
+
+        elif request.POST['action'] == "Sign in!":
+            email = request.POST.get("email")
+            password = request.POST.get("password")
+            user = authenticate(username=email, password=password)
+            if user is not None:
+                login(request, user)
+                return redirect('HomePage')
+            else:
+                messages.error(request, "Login unsuccessful!")
+                return render(request, 'signup.html', context=None)
+
+
+def logout_view(request):
+    logout(request)
+    messages.info(request, "Logged out successfully!")
+    return redirect('LandingPage')
