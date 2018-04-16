1dfddef1c714687c11047ad82975c7014d04d16f
Rohith Kumar <rohit.jaligama@gmail.com>
2018-04-16 15:19:29 +0530

    sending customer url

--- registration/static/commands.js
+++ registration/static/commands.js
@@ -1,4 +1,15 @@
-document.getElementById("loginBtn").onclick= function(){
+document.getElementById("loginBtn").onclick = function(){
 	$('#loginModal').modal('toggle')
+}
+
+function sendData(){
+	var url= document.getElementById("sendURL").value;
+	if ( url.length == 0)
+		alert("Please enter URL");
+	else {
+	$.post('/home/', url, function(result) {
+    alert('successfully posted key1=value1&key2=value2 to foo.php');
+});
+	}
 }
 
