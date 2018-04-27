102df9d62462918dafa39ed4c7d51fdffaf51a01
siddharthnishtala <siddharth.nishtala@gmail.com>
2018-04-27 19:12:55 +0530

    Added Indigo PNR Checking
    Signed-off-by: siddharthnishtala <siddharth.nishtala@gmail.com>

--- traveller/views.py
+++ traveller/views.py
@@ -1,11 +1,47 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from django.conf import settings
+import platform
 
 class TravellerPage(TemplateView):
     def get(self, request, **kwargs):
+        PNR = ""
+        lName = ""
+        print(CheckBooking(PNR, lName))
         return render(request, 'traveller.html', context=None)
 
 
 class VerificationPage(TemplateView):
     def get(self, request, **kwargs):
         return render(request, 'Verification.html', context=None)
+
+def CheckBooking(ref, lName):
+
+    chrome_options = Options()
+    chrome_options.add_argument("--headless")
+
+    if platform.system() == "Darwin":
+        browser = webdriver.Chrome(settings.BASE_DIR + "/chromedriver", chrome_options=chrome_options)
+    elif platform.system() == "Windows":
+        browser = webdriver.Chrome(settings.BASE_DIR + "/chromedriver.exe", chrome_options=chrome_options)
+    else:
+        browser = webdriver.Chrome(settings.BASE_DIR + "/chromedriver_linux", chrome_options=chrome_options)
+
+    browser.get("https://book.goindigo.in/Flight/Map#viewchange")
+
+    refElement = browser.find_element_by_id("indiGoRetrieveBooking_RecordLocator")
+    lNameElement = browser.find_element_by_id("indiGoRetrieveBooking_EmailAddress")
+
+    refElement.send_keys(ref)
+    lNameElement.send_keys(lName)
+
+    submitButton = browser.find_element_by_id("submitButtonId")
+    submitButton.click()
+
+    errorElement = browser.find_elements_by_class_name("errorMsgs")
+    if len(errorElement) == 0:
+        return True
+    else:
+        return False
