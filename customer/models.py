37c1f485e5b5369b9dae43a70dc60bda72e6fe56
siddharthnishtala <siddharth.nishtala@gmail.com>
2018-04-28 06:21:18 +0530

    Added emailing functionality
    Signed-off-by: siddharthnishtala <siddharth.nishtala@gmail.com>

--- customer/models.py
+++ customer/models.py
@@ -1,6 +1,9 @@
 from django.db import models
 from django.contrib.auth.models import User
 from traveller.models import Traveller
+import smtplib as smtp
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
 
 
 class Order(models.Model):
@@ -27,8 +30,76 @@ class Customer(models.Model):
     def __str__(self):
         return "User: " + self.user.username + " | Delivery in: " + self.city
 
+    def save(self, *args, **kwargs):
+        super().save(*args, **kwargs)
+        orderConfEmail(self.user.username)
+
 
 class Connector(models.Model):
     connection_id = models.AutoField(primary_key=True)
     customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
     traveller = models.ForeignKey(Traveller, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return "Customer: " + self.customer.user.username + " | Traveller: " + self.traveller.user.username
+
+    def save(self, *args, **kwargs):
+        super().save(*args, **kwargs)
+        sendEmails(self)
+
+
+def orderConfEmail(id_to):
+    id_from = "parcelpanther@gmail.com"
+    password = "Snu@1234"
+    smtp_server = "smtp.gmail.com"
+    message = MIMEMultipart()
+    message['Subject'] = "Waiting for a traveller"
+    message['from'] = id_from
+    message['to'] = id_to
+    body = "Hi\nYour order has been placed successfully.Order details are shared below.We are looking for a traveller to carry your product.\nWe will get back to you once we find a traveller\n"
+    message.attach(MIMEText(body, 'plain'))
+    server = smtp.SMTP(smtp_server, 587)
+    server.starttls()
+    server.login(id_from, password)
+    server.sendmail(id_from, id_to, message.as_string())
+    server.quit()
+
+
+def sendEmails(connector):
+    customerEmailID = connector.customer.user.username
+    travellerEmailID = connector.traveller.user.username
+
+    updateCustomer(customerEmailID)
+    updateTraveller(travellerEmailID)
+
+def updateCustomer(id_to):
+    id_from = "parcelpanther@gmail.com"
+    password = "Snu@1234"
+    smtp_server = "smtp.gmail.com"
+    message = MIMEMultipart()
+    message['Subject'] = "Traveller found"
+    message['from'] = id_from
+    message['to'] = id_to
+    body = "Hi\nWe have found a traveller who will bw carrying your product.The details of the traveller are shared below\n"
+    message.attach(MIMEText(body, 'plain'))
+    server = smtp.SMTP(smtp_server, 587)
+    server.starttls()
+    server.login(id_from, password)
+    server.sendmail(id_from, id_to, message.as_string())
+    server.quit()
+
+def updateTraveller(id_to):
+    id_from = "parcelpanther@gmail.com"
+    password = "Snu@1234"
+    smtp_server = "smtp.gmail.com"
+    message = MIMEMultipart()
+    message['Subject'] = "We have a product for you to bring back"
+    message['from'] = id_from
+    message['to'] = id_to
+    body = "Hi\nWe are sending you this mail to notify you that you will be bringing back the following product(Details are shared below)\n"
+    message.attach(MIMEText(body, 'plain'))
+    server = smtp.SMTP(smtp_server,587)
+    server.starttls()
+    server.login(id_from, password)
+    server.sendmail(id_from, id_to, message.as_string())
+    server.quit()
--- traveller/models.py
+++ traveller/models.py
@@ -1,5 +1,8 @@
 from django.db import models
 from django.contrib.auth.models import User
+import smtplib as smtp
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
 
 
 class Traveller(models.Model):
@@ -14,3 +17,24 @@ class Traveller(models.Model):
 
     def __str__(self):
         return "User: " + self.user.username + " | Travelling to: " + self.city_of_travel
+
+    def save(self, *args, **kwargs):
+        super().save(*args, **kwargs)
+        registrationConf(self.user.username)
+
+
+def registrationConf(id_to):
+    id_from = "parcelpanther@gmail.com"
+    password = "Snu@1234"
+    smtp_server = "smtp.gmail.com"
+    message = MIMEMultipart()
+    message['Subject'] = "Successfully Registered to PiggyBack"
+    message['from'] = id_from
+    message['to'] = id_to
+    body = "Hi\nYou have been successfully registered\n"
+    message.attach(MIMEText(body, 'plain'))
+    server = smtp.SMTP(smtp_server,587)
+    server.starttls()
+    server.login(id_from,password)
+    server.sendmail(id_from,id_to,message.as_string())
+    server.quit()
